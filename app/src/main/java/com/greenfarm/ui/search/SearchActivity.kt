cbda4e0f626e1ad45a57e53f0b820b67eb1e1fee
crowwan <crowwan@naver.com>
2022-05-05 15:43:03 +0900

    add object-detection model

--- app/src/main/java/com/greenfarm/ui/search/SearchActivity.kt
+++ app/src/main/java/com/greenfarm/ui/search/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.greenfarm.ui.main
 
+import android.content.Intent
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -9,6 +10,7 @@ import com.greenfarm.R
 import com.greenfarm.databinding.ActivityMainBinding
 import com.greenfarm.databinding.ActivitySearchBinding
 import com.greenfarm.ui.BaseActivity
+import com.greenfarm.ui.TestActivity
 
 class SearchActivity: BaseActivity<ActivitySearchBinding>(ActivitySearchBinding::inflate) {
 
@@ -37,6 +39,10 @@ class SearchActivity: BaseActivity<ActivitySearchBinding>(ActivitySearchBinding:
         binding.uploadBackIc.setOnClickListener{
             binding.searchUploadPartView.visibility = View.GONE
             binding.searchCategoryPartView.visibility = View.VISIBLE
+        }
+        binding.uploadStartTv.setOnClickListener{
+            val  intent= Intent(this, TestActivity::class.java)
+            startActivity(intent)
         }
     }
 
